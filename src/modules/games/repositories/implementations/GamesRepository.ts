f67fc132b81cb7e31a85455af591cab4115e17d5
Danilo M. Caldas <magalhaes.danilocaldas@gmail.com>
2021-03-30 23:06:57 -0300

    Challanged completed

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,26 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
+    const games = await this.repository.createQueryBuilder()
+      .where('title ilike :title', {title: `%${param}%`})
+      .getMany()
       // Complete usando query builder
+    
+    return games
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query('SELECT COUNT(id) FROM games'); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
+    return await this.repository
+      .createQueryBuilder('games').innerJoinAndSelect('games.users', 'users').getOne().then(game =>{
+        if(!game){
+          throw new Error('Game not found')
+        }
+        return game.users
+      })
       // Complete usando query builder
   }
 }
